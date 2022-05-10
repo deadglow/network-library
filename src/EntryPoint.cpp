6be0ae1f2dd9af9c9453f4fde0b00d50fb74b876
deadglow <rahul.jhugroo99@gmail.com>
2022-05-11 09:08:32 +1000

    got a client to server connection working -needed to port forward

--- src/EntryPoint.cpp
+++ src/EntryPoint.cpp
@@ -1,16 +1,91 @@
 #include "enet/enet.h"
 #include <iostream>
+#include <string>
 
 #define IS_HOST false
 
 void Host()
 {
+	std::cout << "Creating a server..." << std::endl;
+	ENetAddress hostAddress;
+	hostAddress.host = ENET_HOST_ANY;
+	hostAddress.port = 1234;
 
+	ENetHost* serverHost = enet_host_create(&hostAddress, 32, 2, 0, 0);
+
+	while(true)
+	{
+		ENetEvent receivedEvent;
+
+		while (enet_host_service(serverHost, &receivedEvent, 100) > 0)
+		{
+			std::cout << "Received a packet!" << std::endl;
+			switch (receivedEvent.type)
+			{
+				case ENET_EVENT_TYPE_CONNECT:
+					std::cout << "A new client connected to the server." << std::endl;
+				break;
+				
+				case ENET_EVENT_TYPE_DISCONNECT:
+					std::cout << "A client disconnected from the server." << std::endl;
+				break;
+				
+				case ENET_EVENT_TYPE_RECEIVE:
+					std::cout << "Received user defined packet." << std::endl;
+				break;
+				
+				default:
+					std::cout << "UNKNOWN." << std::endl;
+				break;
+			}
+		}
+	}
 }
 
 void Client()
 {
+	std::cout << "Creating a client..." << std::endl;
 
+	std::cout << "Which IP do you want to connect to?" << std::endl;
+	std::string ipAddress;
+	std::cin >> ipAddress;
+	std::cout << "Attempting to connect to IP: " << ipAddress << " ..." << std::endl;
+
+	ENetHost* clientHost = enet_host_create(nullptr, 1, 2, 0, 0);
+
+	ENetAddress address;
+	address.port = 1234;
+	enet_address_set_host(&address, ipAddress.c_str());
+
+	enet_host_connect(clientHost, &address, 2, 5000);
+
+	while (true)
+	{
+		ENetEvent receivedEvent;
+
+		while (enet_host_service(clientHost, &receivedEvent, 100) > 0)
+		{
+			std::cout << "Client received a packet." << std::endl;
+			switch(receivedEvent.type)
+			{
+				case ENET_EVENT_TYPE_CONNECT:
+					std::cout << "Connected to server!" << std::endl;
+				break;
+				
+				case ENET_EVENT_TYPE_DISCONNECT:
+					std::cout << "Disconnected from server." << std::endl;
+				break;
+				
+				case ENET_EVENT_TYPE_RECEIVE:
+					std::cout << "Received user defined packet." << std::endl;
+				break;
+				
+				default:
+					std::cout << "UNKNOWN." << std::endl;
+				break;
+			}
+		}
+	}
 }
 
 int main()
@@ -23,7 +98,19 @@ int main()
     else
         std::cout << "ENet initialized." << std::endl;
 
-    if (IS_HOST)
+	char input;
+
+	std::cout << "Is this instance a host?" << std::endl;
+
+	std::cin >> input;
+
+	bool isHost = false;
+
+	if (input == 'y')
+		isHost = true;
+
+
+    if (isHost)
     {
         Host();
     }
